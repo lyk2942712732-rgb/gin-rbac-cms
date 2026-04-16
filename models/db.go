241a15759d7c2ea840789c3139a896de13e72e60
lyk2942712732-rgb <lyk2942712732@outlook.com>
2026-04-16 17:41:28 +0800

    数据库完善更改

--- models/db.go
+++ models/db.go
@@ -1,8 +1,10 @@
 package models
 
 import (
+	"log"
 	"myapp/utils"
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -19,16 +21,29 @@ func InitDB() {
 		dsn = "root:675563@tcp(127.0.0.1:3306)/my_gin_db?charset=utf8mb4&parseTime=True&loc=Local"
 	}
 	var err error
-	// gorm.Open 函数用于打开数据库连接，第一个参数是数据库驱动，这里我们使用 mysql.Open 来指定 MySQL 数据库，第二个参数是 gorm.Config 配置对象。
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("连接数据库失败: " + err.Error())
+	maxRetries := 10
+
+	for i := 0; i < maxRetries; i++ {
+
+		// gorm.Open 函数用于打开数据库连接，第一个参数是数据库驱动，这里我们使用 mysql.Open 来指定 MySQL 数据库，第二个参数是 gorm.Config 配置对象。
+		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+		if err == nil {
+			utils.Logger.Info("✅ 数据库连接成功！正在自动迁移表结构...")
+
+			DB.AutoMigrate(&User{}, &Article{}, &Role{}, &Permission{})
+			// GORM 扫描到 many2many 标签时，会自动在数据库里建出 user_roles 和 role_permissions 这两张中间表！
+			// AutoMigrate 方法会根据 User 结构体自动创建或更新数据库表结构，确保数据库中的表与代码中的模型保持一致。
+
+			return
+		}
+
+		log.Printf("⚠️ 数据库连接失败，正在进行第 %d/%d 次重试... (等待 3 秒)", i, maxRetries)
+		time.Sleep(3 * time.Second) // 挂起 3 秒再试
+
 	}
 
-	utils.Logger.Info("✅ 数据库连接成功！正在自动迁移表结构...")
-
-	DB.AutoMigrate(&User{}, &Article{}, &Role{}, &Permission{})
-	// GORM 扫描到 many2many 标签时，会自动在数据库里建出 user_roles 和 role_permissions 这两张中间表！
-	// AutoMigrate 方法会根据 User 结构体自动创建或更新数据库表结构，确保数据库中的表与代码中的模型保持一致。
+	// 如果 10 次（30秒）都没连上,这时候再 panic
+	panic("❌ 数据库连接彻底失败: " + err.Error())
 
 }
