1b457ede45d2c3e4309e47e90eaafb76bdc1e13f
lyk2942712732-rgb <lyk2942712732@outlook.com>
2026-04-17 10:28:40 +0800

    结构体标签修改

--- models/user.go
+++ models/user.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Username string `gorm:"type:varchar(100);uniqueIndex;not null"`
-	Password string `gorm:"not null"`
+	Password string `gorm:"not null" json:"-"` // 密码字段不返回给客户端
 	// 增加这一行，定义用户与角色的多对多关系,
 	Roles []Role `gorm:"many2many:user_roles;" json:"roles"`
 }
